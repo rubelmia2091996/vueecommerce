94b009f4be5f457910fa8a142e95e579c69d2b13
Rubel Mia <rubel.mia2091996@gmail.com>
2024-12-26 02:10:01 +0600

    product list flowbite

--- resources/js/Pages/Admin/Components/AdminLayout.vue
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="antialiased bg-gray-50 dark:bg-gray-900">
         <!-- navber start -->
-         <Navbar/>
+        <Navbar />
         <!-- navber end -->
-
         <!-- start Sidebar -->
-         <Sidebar/>
+        <Sidebar />
         <!-- end Sidebar -->
-
-        <slot />
+        <main class="p-4 md:ml-64 h-auto pt-20">
+            <slot />
+        </main>
     </div>
 </template>
 <script setup>
